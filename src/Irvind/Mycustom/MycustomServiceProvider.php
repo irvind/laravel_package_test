9351c5931691a35314b26f129fc7f1785773d773
Irvind <irvind25@gmail.com>
2015-01-01 23:25:09 +0300

    Test commit

---
+++ src/Irvind/Mycustom/MycustomServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Irvind\Mycustom;
+
+use Illuminate\Support\ServiceProvider;
+
+class MycustomServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('irvind/mycustom');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
